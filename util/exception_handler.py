12b6ff6111f3c88c37214ba5de728f223cfb3511
stupidhacker <minhtran6464.lhp@gmail.com>
2024-10-21 06:57:09 -0400

    fix import module

--- util/exception_handler.py
+++ util/exception_handler.py
@@ -1,6 +1,6 @@
-import asyncio
 import functools
 
+from aiohttp import web
 
 
 def async_exception_handler(func):
@@ -12,7 +12,21 @@ def async_exception_handler(func):
         except Exception as e:
             print(f"An error occurred in function '{func.__name__}': {e}")
             return None  # You can return a default value or re-raise the exception if needed
+
     return wrapper
+
+
+def async_web_response_exception_handler(func):
+    @functools.wraps(func)
+    async def wrapper(*args, **kwargs):
+        # print(f'Jump_in_async_wrapper for {func.__name__}')
+        try:
+            return await func(*args, **kwargs)
+        except Exception as e:
+            return web.json_response(dict(error=f"An error occurred in function '{func.__name__}': {e}"))
+
+    return wrapper
+
 
 def exception_handler(func):
     def wrapper(*args, **kwargs):
@@ -21,4 +35,5 @@ def exception_handler(func):
         except Exception as e:
             print(f"An error occurred in function '{func.__name__}': {e}")
             return None  # You can return a default value or re-raise the exception if needed
+
     return wrapper
